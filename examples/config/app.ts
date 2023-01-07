cdee258ade341c94a6f192310069147a33a7b567
Coderwsq <9699507+wsq__666@user.noreply.gitee.com>
2023-01-07 14:25:42 +0800

    实现create方法创建实例

--- examples/config/app.ts
+++ examples/config/app.ts
@@ -34,3 +34,31 @@ axios({
 }).then(res => {
   console.log(res.data)
 })
+
+
+const instance = axios.create({
+  transformRequest: [(function (data) {
+    return qs.stringify(data)
+  }), ...axios.defaults.transformRequest as AxiosTransformer[]],
+  transformResponse: [...(axios.defaults.transformResponse as AxiosTransformer[]), function (data) {
+    if (typeof data === 'object') {
+      data.b = 2
+    }
+    return data
+  }],
+  url: '/config/post',
+  method: 'post',
+  data: {
+    a: 1
+  }
+})
+
+instance({
+  url: '/config/post',
+  method: 'post',
+  data: {
+    a: 1
+  }
+}).then(res => {
+  console.log(res)
+})
